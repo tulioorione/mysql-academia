324b2159eec506359e5ce4cd3be7a6a53e9ed8f7
Tulio Orione <tulioorione@gmail.com>
2026-04-29 19:11:23 -0300

    feat: create personal table

--- sql/01-create-database.sql
+++ sql/01-create-database.sql
@@ -11,3 +11,15 @@ create table plano (
     valor decimal(10,2) not null,
     duracao_meses int not null
 ) engine InnoDB;
+
+create table personal (
+    id int auto_increment primary key,
+    nome varchar(100) not null,
+    cpf varchar(11) not null unique, 
+    email varchar(100) not null unique,
+    telefone varchar(20) null,
+    data_contratacao date not null,
+    salario decimal(8, 2) not null,
+    especialidade enum('Musculação', 'Crossfit', 'Funcional', 'Pilates', 'Natação') not null,
+    ativo boolean not null default true
+) engine InnoDB;
