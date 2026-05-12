0e4903fd57a5172974acac2987b7421be6c431f1
Tulio Orione <tulioorione@gmail.com>
2026-05-12 19:26:39 -0300

    feat(dml): seed personal table

--- sql/02-insert-data.sql
+++ sql/02-insert-data.sql
@@ -15,3 +15,8 @@ insert into plano (nome, valor, duracao_meses) values
     ('Semestral', 663.00, 6),
     ('Anual', 1170.00, 12);
 
+insert into personal (nome, cpf, email, telefone, data_contratacao, salario, especialidade, ativo) values
+    ('Luciano Souza', '15753436750', 'luciano@academia.com', '33968438125', '2025-09-05', 1899.00, 'Musculação', TRUE),
+    ('Maria Oliveira', '35795985380', 'maria@academia.com', '33934543145', '2025-01-07', 2102.00, 'Crossfit', TRUE),
+    ('Marcos Pereira', '23173215770', 'marcos@academia.com', '33965273015', '2025-09-06', 1830.00, 'Funcional', FALSE);
+    
